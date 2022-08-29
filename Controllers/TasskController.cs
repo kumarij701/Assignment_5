afbac7811c3db4c36a3414e309b3e13a6b08f81d
Kumari <2064588@cognizant.com>
2022-08-29 19:13:03 +0530

    add files

---
+++ Controllers/TasskController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Assignment_5;
+
+namespace Assign_5Pro.Controllers
+{
+    public class TasskController : Controller
+    {
+
+        private readonly TaskssDbContext _context;
+
+        public TasskController()
+        {
+            _context = new TaskssDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Tassk/Add
+        public ActionResult Add()
+        {
+            return View("AddTask");
+        }
+
+
+        [HttpPost]
+        public IActionResult CreateTask([FromBody] Assignment_5.Task task)
+        {
+            _context.Tasks.Add(task);
+            _context.SaveChanges();
+            return Ok();
+            //return RedirectToAction("GetTask" , "Tassk");
+
+        }
+
+        ///Tassk/GetTask
+        [HttpGet]
+        [Route("GetTask")]
+        public IActionResult GetTask()
+        {
+            var tasks = _context.Tasks.ToList();
+            return Ok(tasks);
+            //return View("Tassk",tasks);
+        }
+
+        //[HttpPost("AddTask")]
+        [HttpPost]
+        public IActionResult AddTask([FromBody] Assignment_5.Task task)
+        {
+            _context.Tasks.Add(task);
+            _context.SaveChanges();
+            return Ok();
+            //return RedirectToAction("GetTask" , "Tassk");
+            
+        }
+
+        [HttpDelete]
+        [Route("Delete/{id}")]
+        public ActionResult DeleteTask(int id)
+        {
+            Assignment_5.Task p = _context.Tasks.Find(id);
+           _context.Tasks.Remove(p);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseCors(options => options.WithOrigins("http://localhost:4200/").AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin());
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
